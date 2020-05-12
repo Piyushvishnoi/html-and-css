c806624c5f6fa71168ca2d5efbce46384de6d3c0
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-05-12 23:47:48 +0530

    Update main.js

--- Stone,paper,Scissor game/main.js
+++ Stone,paper,Scissor game/main.js
@@ -1,7 +1,7 @@
 const hands = ["fas fa-hand-rock", "fas fa-hand-paper", "fas fa-hand-scissors"];
 const names = ["rock", "paper", "scissor"];
 const colorwinner = "winner"; 
-let handA = document.getElementById("handA");
+let handA = document.getElementById("handA"); 
 let handB = document.getElementById("handB");
 let nameA = document.getElementById("nameA");
 let nameB = document.getElementById("nameB");
